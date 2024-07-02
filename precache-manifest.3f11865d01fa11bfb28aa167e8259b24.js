06d45ad61db510efda640b73203c4f8c493ed5d9
HyechangLim <okcomper3@gmail.com>
2024-07-02 11:03:54 +0900

    test

---
+++ precache-manifest.3f11865d01fa11bfb28aa167e8259b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc53aaf84c41b38511f7045630a1fc6",
+    "url": "/OrientalMedicineNeoLab/index.html"
+  },
+  {
+    "revision": "12b627a75da10308f178",
+    "url": "/OrientalMedicineNeoLab/static/css/main.27675b6a.chunk.css"
+  },
+  {
+    "revision": "7b2fc0f358a43df87d53",
+    "url": "/OrientalMedicineNeoLab/static/js/2.38f88ae9.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/OrientalMedicineNeoLab/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "12b627a75da10308f178",
+    "url": "/OrientalMedicineNeoLab/static/js/main.4df17b29.chunk.js"
+  },
+  {
+    "revision": "62ac9a2bdf6881ea2036",
+    "url": "/OrientalMedicineNeoLab/static/js/runtime~main.e3374bee.js"
+  }
+]);
